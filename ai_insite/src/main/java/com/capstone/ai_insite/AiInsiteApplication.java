92d0d8a6cd6bc1ead9dde53d022b7f917a4785c4
my2024081062 <ish0817@naver.com>
2026-07-23 16:45:03 +0900

    시작

---
+++ ai_insite/src/main/java/com/capstone/ai_insite/AiInsiteApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.ai_insite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiInsiteApplication.class, args);
+    }
+
+}
---
+++ ai_insite/src/test/java/com/capstone/ai_insite/AiInsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.ai_insite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
