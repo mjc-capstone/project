317655487b9d78efd7777e59de51ae0e0bf011e6
my2024081062 <ish0817@naver.com>
2026-07-28 16:48:30 +0900

    common

---
+++ ai_insite/src/main/java/com/capstone/ai_insite/common/exception/ActiveDataImportJobException.java
@@ -0,0 +1,8 @@
+package com.capstone.ai_insite.common.exception;
+
+public class ActiveDataImportJobException extends RuntimeException {
+
+    public ActiveDataImportJobException(String message) {
+        super(message);
+    }
+}
